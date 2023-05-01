7a0adad1ba346232c3aaede8bfbe4f064cbeb98b
chocobae <chocobae1995@gmail.com>
2023-05-01 15:04:45 -0700

    recent update

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,70 +1,117 @@
 #!/usr/bin/python3
-""" Test module for place.py file. """
+"""
+Unit Test for Place Class
+"""
+import unittest
+from datetime import datetime
+import models
+import json
+import os
 
-from tests.test_models.test_base_model import test_basemodel
-from models.place import Place
+Place = models.place.Place
+BaseModel = models.base_model.BaseModel
+storage_type = os.environ.get('HBNB_TYPE_STORAGE')
 
 
-class test_Place(test_basemodel):
-    """ Test class for place.py """
+class TestPlaceDocs(unittest.TestCase):
+    """Class for testing BaseModel docs"""
 
-    def __init__(self, *args, **kwargs):
-        """ test_Place class constructor"""
-        super().__init__(*args, **kwargs)
-        self.name = "Place"
-        self.value = Place
+    @classmethod
+    def setUpClass(cls):
+        print('\n\n.................................')
+        print('..... Testing Documentation .....')
+        print('........   Place Class   ........')
+        print('.................................\n\n')
 
-    def test_city_id(self):
-        """ Tests the city id """
-        new = self.value()
-        self.assertNotEqual(type(new.city_id), str)
+    def test_doc_file(self):
+        """... documentation for the file"""
+        expected = '\nPlace Class from Models Module\n'
+        actual = models.place.__doc__
+        self.assertEqual(expected, actual)
 
-    def test_user_id(self):
-        """ Tests the user id"""
-        new = self.value()
-        self.assertNotEqual(type(new.user_id), str)
+    def test_doc_class(self):
+        """... documentation for the class"""
+        expected = 'Place class handles all application places'
+        actual = Place.__doc__
+        self.assertEqual(expected, actual)
 
-    def test_name(self):
-        """ Tests the name"""
-        new = self.value()
-        self.assertNotEqual(type(new.name), str)
 
-    def test_description(self):
-        """ Tests the description """
-        new = self.value()
-        self.assertNotEqual(type(new.description), str)
+class TestPlaceInstances(unittest.TestCase):
+    """testing for class instances"""
 
-    def test_number_rooms(self):
-        """ Tests the number of rooms"""
-        new = self.value()
-        self.assertNotEqual(type(new.number_rooms), int)
+    @classmethod
+    def setUpClass(cls):
+        print('\n\n.................................')
+        print('....... Testing Functions .......')
+        print('.........  Place Class  .........')
+        print('.................................\n\n')
 
-    def test_number_bathrooms(self):
-        """ Tests number of bathrooms"""
-        new = self.value()
-        self.assertNotEqual(type(new.number_bathrooms), int)
+    def setUp(self):
+        """initializes new place for testing"""
+        self.place = Place()
 
-    def test_max_guest(self):
-        """ Tests the maximum number of guests"""
-        new = self.value()
-        self.assertNotEqual(type(new.max_guest), int)
+    def test_instantiation(self):
+        """... checks if Place is properly instantiated"""
+        self.assertIsInstance(self.place, Place)
 
-    def test_price_by_night(self):
-        """ Tests the price per night """
-        new = self.value()
-        self.assertNotEqual(type(new.price_by_night), int)
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
+    def test_to_string(self):
+        """... checks if BaseModel is properly casted to string"""
+        my_str = str(self.place)
+        my_list = ['Place', 'id', 'created_at']
+        actual = 0
+        for sub_str in my_list:
+            if sub_str in my_str:
+                actual += 1
+        self.assertTrue(3 == actual)
 
-    def test_latitude(self):
-        """ Tests the latitude of the location """
-        new = self.value()
-        self.assertNotEqual(type(new.latitude), float)
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
+    def test_instantiation_no_updated(self):
+        """... should not have updated attribute"""
+        my_str = str(self.place)
+        actual = 0
+        if 'updated_at' in my_str:
+            actual += 1
+        self.assertTrue(0 == actual)
 
-    def test_longitude(self):
-        """ Tests longitude of house location """
-        new = self.value()
-        self.assertNotEqual(type(new.latitude), float)
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
+    def test_updated_at(self):
+        """... save function should add updated_at attribute"""
+        self.place.save()
+        actual = type(self.place.updated_at)
+        expected = type(datetime.now())
+        self.assertEqual(expected, actual)
 
-    def test_amenity_ids(self):
-        """ Tests the amenity id """
-        new = self.value()
-        self.assertEqual(type(new.amenity_ids), list)
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
+    def test_to_json(self):
+        """... to_json should return serializable dict object"""
+        self.place_json = self.place.to_json()
+        actual = 1
+        try:
+            serialized = json.dumps(self.place_json)
+        except:
+            actual = 0
+        self.assertTrue(1 == actual)
+
+    @unittest.skipIf(storage_type == 'db', 'skip if environ is db')
+    def test_json_class(self):
+        """... to_json should include class key with value Place"""
+        self.place_json = self.place.to_json()
+        actual = None
+        if self.place_json['__class__']:
+            actual = self.place_json['__class__']
+        expected = 'Place'
+        self.assertEqual(expected, actual)
+
+    def test_guest_attribute(self):
+        """... add guest attribute"""
+        self.place.max_guest = 3
+        if hasattr(self.place, 'max_guest'):
+            actual = self.place.max_guest
+        else:
+            actual = ''
+        expected = 3
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main
